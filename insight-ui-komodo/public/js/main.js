b831a8d1c65a17de0df464c4ad382b2d9e0a4d40
ciphs <0x0@ciphs.org>
2021-05-15 08:09:57 +0800

    disable ads

--- insight-ui-komodo/public/js/main.js
+++ insight-ui-komodo/public/js/main.js
@@ -23,7 +23,6 @@ angular.module('insight', [
   'insight.search',
   'insight.charts',
   'insight.status',
-  'insight.ad',
   'insight.connection',
   'insight.currency',
   'insight.messages'
@@ -38,96 +37,12 @@ angular.module('insight.address', []);
 angular.module('insight.search', []);
 angular.module('insight.charts', []);
 angular.module('insight.status', []);
-angular.module('insight.ad', []);
 angular.module('insight.connection', []);
 angular.module('insight.currency', []);
 angular.module('insight.messages', []);
 
 // Source: public/src/js/controllers/ad.js
-angular
-  .module("insight.ad")
-  .controller("AdController", function ($scope, $http) {
-    $http
-      .get("https://kmd-data.s3.us-east-2.amazonaws.com/info/ecosystem.json")
-      .success(function (data, status, headers, config) {
-        ads = {};
-        priorities = {};
-        data.forEach(function (project) {
-          ads[project.projectName] = project.ads;
-          priorities[project.projectName] = project.priority;
-        });
-        // finding the project whose ad has to be displayed now
-        for (var key in priorities) {
-          if (priorities[key] == "always") {
-            var project = key;
-          } else if (priorities[key] == "never") {
-            delete priorities[key];
-          }
-        }
-        if (!project) {
-          var sum = 0;
 
-          for (var key in priorities) {
-            sum += parseFloat(priorities[key]);
-          }
-          ranges = {};
-          rangeEnd = 1;
-          for (var key in priorities) {
-            ranges[key] = [
-              rangeEnd,
-              rangeEnd + parseFloat(priorities[key] - 1)
-            ];
-            rangeEnd = rangeEnd + parseFloat(priorities[key]);
-          }
-          rand = Math.floor(Math.random() * sum) + 1;
-          for (var key in ranges) {
-            if (rand >= ranges[key][0] && rand <= ranges[key][1]) {
-              var project = key;
-            }
-          }
-        }
-        // finding the ad in the selected project
-
-        ads = ads[project];
-        allAds = [];
-        var sum = 0;
-        for (var i = 0; i < ads.length; i++) {
-          if (ads[i].frequency == "always") {
-            var toDisplay = ads[i].data;
-          } else if (ads[i].frequency == "never") {
-
-          } else {
-            allAds.push(ads[i]);
-          }
-        }
-
-
-        if (!toDisplay) {
-          for (var i = 0; i < allAds.length; i++) {
-            sum += parseFloat(allAds[i].frequency);
-          }
-          ranges = [];
-          rangeEnd = 1;
-          for (i = 0; i < allAds.length; i++) {
-            ranges[i] = [
-              rangeEnd,
-              rangeEnd + parseFloat(allAds[i].frequency - 1)
-            ];
-            rangeEnd = rangeEnd + parseFloat(allAds[i].frequency);
-          }
-          rand = Math.floor(Math.random() * sum) + 1;
-          for (i = 0; i < ranges.length; i++) {
-            if (rand >= ranges[i][0] && rand <= ranges[i][1]) {
-              var toDisplay = allAds[i].data;
-            }
-          }
-        }
-        $scope.toDisplay = toDisplay;
-      })
-      .error(function () {
-        $scope.error = true;
-      });
-  });
 // Source: public/src/js/controllers/address.js
 angular.module('insight.address').controller('AddressController',
   function($scope, $rootScope, $routeParams, $location, Global, Address, getSocket) {
